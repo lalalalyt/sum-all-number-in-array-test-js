e878980524e2e0b2173d87c86a4d0e188ea67bc1
lalalalyt <lynneli318@gmail.com>
2022-02-06 19:57:14 -0500

    sum the numbers from an array

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let sum = 0 
+  array.forEach(element => { 
+    if (Array.isArray(element)) {
+    sum += sumItems(element)
+    }
+    else { sum += element}
+  });
+  return sum
 }
 
+console.log(sumItems([1, 2, 3, 4, 5]))
+console.log(sumItems([[1, 2, [[3], 4]], 5, []]))
+console.log(sumItems([[[[[[[[[[[[[1]]]]]]]]]]]]]))
 module.exports = sumItems;
